5fbe5808d1738282fbd53e707e2e20a74f9b8f51
Zaky Athahillah <zaky1472@gmail.com>
2026-05-01 19:22:29 +0700

    bug fix

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:3,1');
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
 
 Route::middleware(['jwt', 'throttle:120,1'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
