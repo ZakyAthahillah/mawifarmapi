3ce75f6d1749b82ca86b09c89a52519ddef9cf77
Zaky Athahillah <zaky1472@gmail.com>
2026-05-01 19:25:22 +0700

    bug fix

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => array_values(array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://192.168.11.7:3000'))))),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+    'exposed_headers' => ['Retry-After', 'X-RateLimit-Limit', 'X-RateLimit-Remaining', 'X-RateLimit-Reset'],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
